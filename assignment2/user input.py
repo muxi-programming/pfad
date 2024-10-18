510aae80ff7c3bc5b5ecb88bd300ed26fa49fb3a
muxi-programming <dengyue0922@163.com>
2024-10-18 18:48:04 +0800

    assignment2

---
+++ assignment2/user input.py
@@ -0,0 +1,7 @@
+name = input("What's your name? ")
+print(name, "Hello!")
+age = input("How old are you? ")
+my_age = int(age) + 2
+print("I am 2 years older than you! I am", str(my_age))
+movie = input("What's your favourite game? ")
+print(f"I like {movie} too! The game mechanics are pretty interesting!")
